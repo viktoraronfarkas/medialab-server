5a82655a2b63e07039c5d284d25e28a2d74fc08d
Viktor Farkas <viktoraron.farkas@gmail.com>
2023-06-15 15:56:57 +0200

    update

--- config/database.js
+++ config/database.js
@@ -1,11 +1,10 @@
-const mysql = require('mysql');
-require('dotenv').config();
+const mysql = require("mysql");
 
 const pool = mysql.createPool({
-  host: 'atp.fhstp.ac.at', // Your connection address (localhost).
+  host: "atp.fhstp.ac.at", // Your connection address (localhost).
   port: 8007,
-  user: 'uasync', // Your database's username.
+  user: "uasync", // Your database's username.
   password: process.env.DB_PASSWORD,
-  database: 'uasync',
+  database: "uasync",
 });
 module.exports = pool;
